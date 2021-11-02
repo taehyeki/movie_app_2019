4f4b727138246f9ff89d720820b09444311eff55
tekkenkim <gmrrltkxogus@naver.com>
2021-11-02 21:16:38 +0900

    modify 2021 -> 2019

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Movie App 2021
+# Movie App 2019
 
 React JS Fundamentals Course 2019 updated
--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail.js";
 import Navigator from "./components/Navigator.js";
+
 import { HashRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <Navigator />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
-    </HashRouter>
+    <>
+      <HashRouter>
+        <Navigator />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/movie/:id" component={Detail} />
+      </HashRouter>
+    </>
   );
 }
 
